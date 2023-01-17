3247c2cad5945b4a3660224e274f2ff6069c3c38
Alex <oleksii120@gmail.com>
2023-01-17 19:30:05 +0200

    Created ViewModel for ViewEmail window, started working on functionality. Replaced old WebBrowser item with modern WebView

---
+++ IMAP Client/ViewModel/ViewEmailWindowViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Web.WebView2.WinForms;
+using MimeKit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMAP_Client.ViewModel
+{
+    public class ViewEmailWindowViewModel
+    {
+        public WebView2 Browser { get; }
+        public MimeMessage? Message { get; private set; }
+
+        public ViewEmailWindowViewModel()
+        {
+            Browser = new();
+        }
+        public void Load(MimeMessage msg)
+        {
+            Message = msg;
+            Browser.NavigateToString(msg.HtmlBody);
+        }
+    }
+}
